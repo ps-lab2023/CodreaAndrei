785ba470fbca3ff1e281a16a1332942f03f52ced
Andrei-00 <andrei.codrea00@gmail.com>
2023-04-10 22:41:08 +0300

    Model ready + tests

--- src/main/java/com/example/tradingCards/DTO/UserDTO.java
+++ src/main/java/com/example/tradingCards/DTO/UserDTO.java
@@ -1,4 +1,21 @@
 package com.example.tradingCards.DTO;
 
+import com.example.tradingCards.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class UserDTO {
+
+    private Long id;
+
+    private String name;
+    private String password;
+    // Enum from User might need to be included
+    private User.Type role;
+
 }
